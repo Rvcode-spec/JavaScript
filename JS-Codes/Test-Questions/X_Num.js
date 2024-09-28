14c8f50e766e19cd7b04cbb2af9967f30eafaa9e
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-09-28 12:05:07 +0530

    Updata

--- JS-Codes/Test-Questions/X_Num.js
+++ JS-Codes/Test-Questions/X_Num.js
@@ -1,11 +1,14 @@
 let arr="9871585013";
 let ans = "";
 for(let i=0; i<arr.length; i++){
-    ans+=arr[i]
+    let nms=Number(arr[i])
+    
     if(i%2!==0){
-        ans+='x'  
+        ans+='x';
+    }else{
+        ans+=nms;
     }
-
+   
     
 }
 console.log(ans);
