59e98a7544d6ba5f8efffbf0d8f93872c19bbf6e
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-10-03 14:08:58 +0530

    Conditions

---
+++ Day-2/Conditions/If-else/Que-11.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program that displays the largest integer among two integers.
+
+let a=87, b=67;
+if(a>b){
+    console.log(a);
+}else if(a<b){
+    console.log(b);
+    
+}
---
+++ Day-2/Conditions/If-else/Que-12.js
@@ -0,0 +1,16 @@
+// Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the results.
+
+// let x=1, y=5, z=0;
+
+//     if(x>y){
+//         console.log(x);
+//     }else if (x<y){
+//         console.log(y);
+//     }else if(y>z){
+//         console.log(y);
+//     }else if(y<z){
+//         console.log(z);
+        
+//     }
+    
+// console.log(x,y,z);
