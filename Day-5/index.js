fd2adc83a4fba046619c903fa5edf780d31a392e
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-10-30 21:17:49 +0530

    Start the function

---
+++ Day-5/index.js
@@ -0,0 +1,13 @@
+// function 
+function checktheevenoldNo(num1){
+
+    if(num1%2==0){
+        console.log("even", num1);
+    }else{
+        console.log("old", num1);
+        
+    }
+
+    
+}
+checktheevenoldNo(50)
