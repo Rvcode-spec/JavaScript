4fcb9a9213155a5d7fbc6867621868ccfd94d42e
Ravi Shankar Singh <civilstudy2024@gmail.com>
2025-01-10 23:21:28 +0530

    New Work

---
+++ JS/Modules-1/Chapter-1/index.js
@@ -0,0 +1,30 @@
+console.log('Hello World!');
+alert('Hello World!');
+
+console.log("With new lines but without indentation:");
+
+
+let status = "new";
+let scared = true;
+if (status === "new") {
+console.log("Welcome to JavaScript!");
+if (scared) {
+console.log("Don't worry you will be fine!");
+} else {
+console.log("You're brave! You are going to do great!");
+}
+} else {
+console.log("Welcome back, I knew you'd like it!");
+}
+
+prompt("Hi! How are you?");
+
+// Random numbers
+
+console.log(Math.random());
+
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+
+
+
