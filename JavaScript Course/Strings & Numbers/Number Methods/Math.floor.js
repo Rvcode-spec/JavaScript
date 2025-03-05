5e154081db75bf8c46ba9273e8a941b8552fc5d9
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-03-06 04:15:02 +0530

    String & Number finished

---
+++ JavaScript Course/Strings & Numbers/Number Methods/Math.floor.js
@@ -0,0 +1,7 @@
+// Math.floor() always rounds a number down to the nearest integer.
+
+console.log(Math.floor(4.9));  // Output: 4
+console.log(Math.floor(4.1));  // Output: 4
+console.log(Math.floor(4.0));  // Output: 4
+console.log(Math.floor(-4.3)); // Output: -5
+console.log(Math.floor(-4.9)); // Output: -5
